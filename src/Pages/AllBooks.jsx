53d7887495964431a1acb0d0fc65f888891b1a6a
kennedyowusu <kennediowusu@gmail.com>
2023-01-21 09:34:07 +0000

    use props to pass data between components

--- src/Pages/AllBooks.jsx
+++ src/Pages/AllBooks.jsx
@@ -2,9 +2,36 @@ import React from 'react';
 import InputForm from '../components/Books/InputForm';
 import ShowBook from '../components/Books/ShowBook';
 
+const books = [
+  {
+    id: 1,
+    title: 'The Hobbit',
+    author: 'J.R.R. Tolkien',
+  },
+  {
+    id: 2,
+    title: 'The Hobbit',
+    author: 'J.R.R. Tolkien',
+  },
+  {
+    id: 3,
+    title: 'The Hobbit',
+    author: 'J.R.R. Tolkien',
+  },
+  {
+    id: 4,
+    title: 'The Hobbit',
+    author: 'J.R.R. Tolkien',
+  },
+];
+
 const AllBooks = () => (
   <div>
-    <ShowBook />
+    {
+      books.map((book) => (
+        <ShowBook key={book.id} title={book.title} author={book.author} />
+      ))
+    }
     <InputForm />
   </div>
 );
