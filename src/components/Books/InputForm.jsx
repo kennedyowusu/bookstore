8f1c21bf6666eb3099ebb7d66a7f0a222c77ba01
kennedyowusu <kennediowusu@gmail.com>
2023-01-24 01:39:54 +0000

    resolve bug

--- src/components/Books/InputForm.jsx
+++ src/components/Books/InputForm.jsx
@@ -3,70 +3,55 @@ import { useDispatch } from 'react-redux';
 import { addBook } from '../../redux/books/books';
 
 const InputForm = () => {
-  const [state, setState] = useState({
-    id: 0,
-    title: '',
-    author: '',
-    category: '',
-  });
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [category, setCategory] = useState('Action');
+  const [validateForm, setValidateForm] = useState(false);
+
   const dispatch = useDispatch();
 
-  // Validate input fields
-  const validate = () => {
-    if (state.title && state.author && state.category) {
-      return true;
-    }
-    return false;
-  };
-
-  const handleBookAddition = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    dispatch(addBook(state));
-    // validate input fields
-    if (validate()) {
-      setState({
-        id: state.id + 1,
-        title: '',
-        author: '',
-        category: '',
-      });
+    if (title === '' || author === '') {
+      setValidateForm(true);
+    } else {
+      setValidateForm(false);
+      dispatch(addBook({ title, author, category }));
+      setTitle('');
+      setAuthor('');
+      setCategory('Action');
     }
   };
 
   const handleTitleChange = (e) => {
-    setState({ ...state, title: e.target.value });
+    setTitle(e.target.value);
   };
 
   const handleAuthorChange = (e) => {
-    setState({ ...state, author: e.target.value });
+    setAuthor(e.target.value);
   };
 
   return (
     <div>
-      <form action="" onSubmit={handleBookAddition}>
-        <div>
-          <input
-            type="text"
-            placeholder="Enter Title"
-            onChange={handleTitleChange}
-            value={state.title}
-          />
-        </div>
-        <div>
-          <input
-            type="text"
-            placeholder="Enter Author"
-            onChange={handleAuthorChange}
-            value={state.author}
-          />
-        </div>
-        <select>
+      <form action="" onSubmit={handleSubmit}>
+        <input type="text" placeholder="Title" value={title} onChange={handleTitleChange} />
+        <input type="text" placeholder="Author" value={author} onChange={handleAuthorChange} />
+        <select name="category" id="category" value={category} onChange={(e) => setCategory(e.target.value)}>
           <option value="Action">Action</option>
           <option value="Biography">Biography</option>
           <option value="History">History</option>
-          <option value="Horror">Horror</option>
+          <option value="Sci-Fi">Sci-Fi</option>
         </select>
         <button type="submit">Add Book</button>
+        {
+          validateForm
+            ? (
+              <p className="error" style={{ color: 'red' }}>
+                Please fill in all fields
+              </p>
+            )
+            : null
+        }
       </form>
     </div>
   );
